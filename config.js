04ed902b8851245602d17205be29498fefbe7eb9
Benjamin Thomas Schwertfeger <development@b-schwertfeger.de>
2022-05-20 10:07:51 +0200

    stash

--- config.js
+++ config.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 const ip = require('underscore').chain(require('os').networkInterfaces()).values().flatten().filter((val) => {
     return (val.family == 'IPv4' && val.internal == false)
 }).pluck('address').first().value();
-console.log(ip)
+
+if (typeof ip === 'undefined') console.log('IP not found, use "ifconfig" in SHELL to find local ip of this device.')
+else console.log(`IP: ${ip};`)
 
 module.exports.DEVELOPMENT = {
     PORT: 3001,
